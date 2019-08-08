3551d1a7a7893596fe67ca3d1dd68b154ba86e6a
Nikhil Tyagi <nityagi@akamai.com>
2019-08-09 04:09:32 +0530

    user location fetch

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
 const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore?&client_id=FPRD2S2RFIB4QLBNBBHNAMLYOUF2AZSZ21ZK53QYASWCRJ1Z&client_secret=FEFA44EG0YDZ0XKA1UWX5ZWLZJLE30E2GYRLGB44PKE5KZ0E&v=20170915";
 
-//press on submit button and scroll to results
-const scrollPageTo = (myTarget, topPadding) => {
-    if (topPadding == undefined) {
-        topPadding = 0;
+
+let state = {
+    radius: 500, //default value
+    currentLocation: {},
+    category: "TOPPICKS"
+    
+};
+
+const findCurrentLocation = () => {
+    if (navigator.geolocation) {
+        // Call getCurrentPosition with success and failure callbacks
+        navigator.geolocation.getCurrentPosition((position) => {
+            state.currentLocation.lng = position.coords.longitude;
+            state.currentLocation.lat = position.coords.latitude;
+            $('.navigation').removeClass("hide");
+            $('#radius').removeClass("hide");
+            render();
+        });
     }
-    var moveTo = $(myTarget).offset().top - topPadding;
-    $('html, body').stop().animate({
-        scrollTop: moveTo
-    }, 200);
+}
+
+
+const render = () => {
+    $("#radius").change(function(e){
+        changeRadius(e);               
+    });
+    $('.category-button').click(function () {
+        let city = $('.search-query').val();
+        state.category = $(this).text();
+        fetchvenues(state.currentLocation.lat, state.currentLocation.lng, state.radius, state.category);
+    });
+    fetchvenues(state.currentLocation.lat, state.currentLocation.lng, state.radius);
 };
 
 //retrieve data from FourSquare API
-const getFourSquareData = () => {
-    $('.category-button').click(function () {
-        let city = $('.search-query').val();
-        let category = $(this).text();
-        $.ajax(FOURSQUARE_SEARCH_URL, {
+const getFourSquareData = (lat, lng, radius) => {
+    fetchvenues(state.currentLocation.lat, state.currentLocation.lng, state.radius);
+};
+
+
+const fetchvenues = (lat, lng, radius, category) => {
+    
+     var section = "TOPPICKS";
+     if( category){
+         section = category;
+     }
+     $.ajax(FOURSQUARE_SEARCH_URL, {
             data: {
-                near: city,
+                ll: `${lat},${lng}`,
+                radius,
+                // near: city,
                 venuePhotos: 1,
-                limit: 9,
+                limit: 20,
                 query: 'recommended',
-                section: category,
+                section: section
             },
             dataType: 'json',
             type: 'GET',
             success: function (data) {
                 try {
+                    if(data.response.groups[0].items.length === 0) {
+                        $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
+                        return;
+                    } 
                     let results = data.response.groups[0].items.map(function (item, index) {
                         return displayResults(item);
                     });
                     $('#foursquare-results').html(results);
-                    scrollPageTo('#foursquare-results', 15);
+                    //scrollPageTo('#foursquare-results', 15);
                 } catch (e) {
-                    $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
+                    $('#foursquare-results').html("<div class='result'><p> Error occurred while retreiving data! </p></div>");
                 }
             },
             error: function (e) {
                 $('#foursquare-results').html(`<div class='result'><p>${e.responseJSON.meta.errorDetail}</p></div>`);
             }
         });
-    });
-};
+}
 
 /**
 <div class="result-image" style="background-image: url(https://igx.4sqi.net/img/general/width960${result.venue.photos.groups[0].items[0].suffix})" ;>
@@ -56,7 +91,7 @@ const displayResults = (result) => {
     <div class="result col-3">
         
         <div class="result-description">
-            <h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
+            <h2 class="result-name">${result.venue.name}</h2>
             <span class="icon">
                 <img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
             </span>
@@ -72,29 +107,12 @@ const displayResults = (result) => {
 };
 
 
-const enterLocation = () => {
-    document.querySelector('.category-button').click(function () {
-        $('button').removeClass("selected");
-        $(this).addClass("selected");
-    });
-
-    $('.search-form').submit(function (event) {
-        event.preventDefault();
-        $('.navigation').removeClass("hide");
-        $('#foursquare-results').html("");
-        getFourSquareData();
-        $('button').removeClass("selected");
-    });
-};
-
-//autocomplete location name in form
-function activatePlacesSearch() {
-    let options = {
-        types: ['(regions)']
-    };
-    let input = document.querySelector('#search-term');
-    let autocomplete = new google.maps.places.Autocomplete(input, options);
+const changeRadius = (event) => {
+    console.log("event triggered");
+    state.radius = parseInt(event.target.value);
+    fetchvenues(state.currentLocation.lat, state.currentLocation.lng, state.radius);
 }
 
-
-enterLocation();
+window.addEventListener('load', function () {
+    findCurrentLocation();
+});
